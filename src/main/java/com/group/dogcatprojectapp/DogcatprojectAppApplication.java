f6838b34e0a591a49464e3885148c72b7a6c9a65
kojihye <natalie429@naver.com>
2023-02-25 12:58:34 +0900

    첫 번째 커밋

---
+++ src/main/java/com/group/dogcatprojectapp/DogcatprojectAppApplication.java
@@ -0,0 +1,15 @@
+package com.group.dogcatprojectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogcatprojectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogcatprojectAppApplication.class, args);
+		System.out.println("Server is running. . .");
+	}
+
+
+}
---
+++ src/test/java/com/group/dogcatprojectapp/DogcatprojectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.dogcatprojectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogcatprojectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
